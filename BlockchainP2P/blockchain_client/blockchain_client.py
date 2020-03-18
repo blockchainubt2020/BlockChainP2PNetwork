a8f198db0c473d3914fa4aa3493167810240072f
Egzon <docpllana@gmail.com>
2020-03-19 00:17:18 +0100

    Finished -> Viewing the Transactions Available in the Node
    Final parts finished by Kujtim Krasniqi.

--- BlockchainP2P/blockchain_client/blockchain_client.py
+++ BlockchainP2P/blockchain_client/blockchain_client.py
@@ -1,47 +1,89 @@
-from flask import Flask, render_template
-from time import time
-
-class Blockchain:
-
-    def __init__(self):
-        self.transactions = []
-        self.chain = []
-        # Create the genesis block
-        self.create_block(0, '00')
-
-    def create_block(self, nonce, previous_hash):
-        """
-        Add a block of transactions to the blockchain
-        """
-        block = {'block_number': len(self.chain) + 1,
-                 'timestamp': time(),
-                 'transactions': self.transactions,
-                 'nonce': nonce,
-                 'previous_hash': previous_hash}
-
-        # Reset the current list of transactions
-        self.transactions = []
-        self.chain.append(block)
+from flask import Flask, request, jsonify, render_template
+import Crypto
+import Crypto.Random
+from Crypto.PublicKey import RSA
+import binascii
+from collections import OrderedDict
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
 
 
+class Transaction:
 
-# Instantiate the Blockchain
-blockchain = Blockchain()
+    def __init__(self, sender_public_key, sender_private_key, recipient_public_key, amount):
+        self.sender_public_key = sender_public_key
+        self.sender_private_key = sender_private_key
+        self.recipient_public_key = recipient_public_key
+        self.amount = amount
 
-# Instantiate the Node
+    def to_dict(self):
+        return OrderedDict({
+            'sender_public_key': self.sender_public_key,
+            'recipient_public_key': self.recipient_public_key,
+            'amount': self.amount
+        })
+
+    def sign_transaction(self):
+        private_key = RSA.importKey(
+            binascii.unhexlify(self.sender_private_key))
+        signer = PKCS1_v1_5.new(private_key)
+        hash = SHA.new(str(self.to_dict()).encode('utf8'))
+        return binascii.hexlify(signer.sign(hash)).decode('ascii')
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('./index.html')
+    return render_template('index.html')
+
+
+@app.route('/generate/transaction', methods=['POST'])
+def generate_transaction():
+    sender_public_key = request.form['sender_public_key']
+    sender_private_key = request.form['sender_private_key']
+    recipient_public_key = request.form['recipient_public_key']
+    amount = request.form['amount']
+
+    transaction = Transaction(
+        sender_public_key, sender_private_key, recipient_public_key, amount)
+
+    response = {'transaction': transaction.to_dict(),
+                'signature': transaction.sign_transaction()
+                }
+    return jsonify(response), 200
+
+
+@app.route('/make/transaction')
+def make_transaction():
+    return render_template('make_transaction.html')
+
+
+@app.route('/view/transactions')
+def view_transaction():
+    return render_template('view_transactions.html')
+
+
+@app.route('/wallet/new')
+def new_wallet():
+    random_gen = Crypto.Random.new().read
+    private_key = RSA.generate(1024, random_gen)
+    public_key = private_key.publickey()
+
+    response = {
+        'private_key': binascii.hexlify(private_key.export_key(format('DER'))).decode('ascii'),
+        'public_key': binascii.hexlify(public_key.export_key(format('DER'))).decode('ascii')}
+
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5001, type=int, help="port to listen to")
+    parser.add_argument('-p', '--port', default=8081,
+                        type=int, help="port to listen to")
     args = parser.parse_args()
     port = args.port
 
